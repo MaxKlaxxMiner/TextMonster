8d7984affff9ed043fc0596307c1e1ccb71ffe20
MaxKlaxxMiner <max@tetrisk.de>
2015-06-17 12:45:39 +0200

    Struct "TextPos" hinzugefügt

---
+++ TextMonsterSystem/Core/TextPos.cs
@@ -0,0 +1,23 @@
+﻿#region # using *.*
+
+using TextMonsterSystem.Memory;
+
+#endregion
+
+namespace TextMonsterSystem.Core
+{
+  /// <summary>
+  /// merkt sich die Speicherposition in einer TextMemory-Klasse
+  /// </summary>
+  public struct TextPos
+  {
+    /// <summary>
+    /// merkt sich die ZeilenNummer (beginnend bei 1)
+    /// </summary>
+    internal long line;
+    /// <summary>
+    /// merkt sich die Spaltennummer (beginnend bei 1)
+    /// </summary>
+    internal long column;
+  }
+}
