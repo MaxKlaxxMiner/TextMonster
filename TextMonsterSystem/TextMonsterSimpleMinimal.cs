6943049e8801ade7a251903aa7f2eaac207dfdf7
MaxKlaxx <max@tetrisk.de>
2015-06-20 20:53:36 +0200

    Refactor: fehlende region-blöcke bei den usings nachgetragen

--- TextMonsterSystem/TextMonsterSimpleMinimal.cs
+++ TextMonsterSystem/TextMonsterSimpleMinimal.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿#region # using *.*
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TextMonsterSystem.Core;
 using TextMonsterSystem.Memory;
+
+#endregion
 
 namespace TextMonsterSystem
 {
--- TextMonsterTests/TextMonsterBasicsTests.cs
+++ TextMonsterTests/TextMonsterBasicsTests.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿#region # using *.*
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TextMonsterSystem;
+
+#endregion
 
 namespace TextMonsterTests
 {
