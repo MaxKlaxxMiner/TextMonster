202bbf391423bfb4a3ddce25f86c8ae84f879d48
MaxKlaxxMiner <max@tetrisk.de>
2018-08-21 22:55:34 +0200

    Konsolenprogramm hinzugefügt zum Debuggen und testen von Textmonster-Funktionen

---
+++ TestTool/Program.cs
@@ -0,0 +1,17 @@
+﻿#region # using *.*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#endregion
+
+namespace TestTool
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}
