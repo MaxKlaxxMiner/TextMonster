6a991f938ff0ac4a3d7ea6483f36e5762de1cc1c
MaxKlaxxMiner <max@tetrisk.de>
2018-08-22 20:55:04 +0200

    TestTool: Autogenerierung aller Test-Dateien

--- TestTool/Program.cs
+++ TestTool/Program.cs
@@ -10,21 +10,28 @@ namespace TestTool
 {
   class Program
   {
+    static void CreateTestFiles(long size)
+    {
+      Console.WriteLine(TestFile.CreateFilePrime(TestFile.FileType.Binary, size));
+      Console.WriteLine(TestFile.CreateFilePrime(TestFile.FileType.Text, size));
+      Console.WriteLine(TestFile.CreateFilePrime(TestFile.FileType.Xml, size));
+      Console.WriteLine(TestFile.CreateFilePrime(TestFile.FileType.Json, size));
+      Console.WriteLine(TestFile.CreateFilePrime(TestFile.FileType.Tsv, size));
+      Console.WriteLine(TestFile.CreateFilePrime(TestFile.FileType.Csv, size));
+    }
+
+    static void CreateTestFiles()
+    {
+      CreateTestFiles(1);         // 1 Byte
+      CreateTestFiles(100);       // 100 Bytes
+      CreateTestFiles(10000);     // 10 kByte
+      CreateTestFiles(1000000);   // 1 MByte
+      CreateTestFiles(100000000); // 100 MByte
+    }
+
     static void Main(string[] args)
     {
-      long len = 1;
-      for (; ; )
-      {
-        Console.Write("create {0:N2} kByte Files:", len / 1024.0);
-        Console.ReadLine();
-        Console.WriteLine(TestFile.CreateFilePrime(TestFile.FileType.Binary, len));
-        Console.WriteLine(TestFile.CreateFilePrime(TestFile.FileType.Text, len));
-        Console.WriteLine(TestFile.CreateFilePrime(TestFile.FileType.Xml, len));
-        Console.WriteLine(TestFile.CreateFilePrime(TestFile.FileType.Json, len));
-        Console.WriteLine(TestFile.CreateFilePrime(TestFile.FileType.Tsv, len));
-        Console.WriteLine(TestFile.CreateFilePrime(TestFile.FileType.Csv, len));
-        len *= 10;
-      }
+      CreateTestFiles();
     }
   }
 }
