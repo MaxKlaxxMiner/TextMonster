38af6804ebbc3ff15672e421082c3d7d9fc2f160
MaxKlaxxMiner <max@tetrisk.de>
2018-09-05 01:44:40 +0200

    5728

---
+++ TextMonster/Xml/Xml_Reader/XmlSerializationGeneratedCode.cs
@@ -0,0 +1,39 @@
+﻿namespace TextMonster.Xml.Xml_Reader
+{
+  /// <include file='doc\XmlSerializationGeneratedCode.uex' path='docs/doc[@for="XmlSerializationGeneratedCode"]/*' />
+  ///<internalonly/>
+  public abstract class XmlSerializationGeneratedCode
+  {
+    TempAssembly tempAssembly;
+    int threadCode;
+    ResolveEventHandler assemblyResolver;
+
+    internal void Init(TempAssembly tempAssembly)
+    {
+      this.tempAssembly = tempAssembly;
+      // only hook the assembly resolver if we have something to help us do the resolution
+      if (tempAssembly != null && tempAssembly.NeedAssembyResolve)
+      {
+        // we save the threadcode to make sure we don't handle any resolve events for any other threads
+        threadCode = Thread.CurrentThread.GetHashCode();
+        assemblyResolver = new ResolveEventHandler(OnAssemblyResolve);
+        AppDomain.CurrentDomain.AssemblyResolve += assemblyResolver;
+      }
+    }
+
+    // this method must be called at the end of serialization
+    internal void Dispose()
+    {
+      if (assemblyResolver != null)
+        AppDomain.CurrentDomain.AssemblyResolve -= assemblyResolver;
+      assemblyResolver = null;
+    }
+
+    internal Assembly OnAssemblyResolve(object sender, ResolveEventArgs args)
+    {
+      if (tempAssembly != null && Thread.CurrentThread.GetHashCode() == threadCode)
+        return tempAssembly.GetReferencedAssembly(args.Name);
+      return null;
+    }
+  }
+}
---
+++ TextMonster/Xml/Xml_Reader/XmlSerializerImplementation.cs
@@ -0,0 +1,25 @@
+﻿namespace TextMonster.Xml.Xml_Reader
+{
+  /// <include file='doc\XmlSerializer.uex' path='docs/doc[@for="XmlSerializerImplementation"]/*' />
+  ///<internalonly/>
+  /// <devdoc>
+  ///    <para>[To be supplied.]</para>
+  /// </devdoc>
+  public abstract class XmlSerializerImplementation
+  {
+    /// <include file='doc\XmlSerializer.uex' path='docs/doc[@for="XmlSerializerImplementation.Reader"]/*' />
+    public virtual XmlSerializationReader Reader { get { throw new NotSupportedException(); } }
+    /// <include file='doc\XmlSerializer.uex' path='docs/doc[@for="XmlSerializerImplementation.Writer"]/*' />
+    public virtual XmlSerializationWriter Writer { get { throw new NotSupportedException(); } }
+    /// <include file='doc\XmlSerializer.uex' path='docs/doc[@for="XmlSerializerImplementation.ReadMethods"]/*' />
+    public virtual Hashtable ReadMethods { get { throw new NotSupportedException(); } }
+    /// <include file='doc\XmlSerializer.uex' path='docs/doc[@for="XmlSerializerImplementation.WriteMethods"]/*' />
+    public virtual Hashtable WriteMethods { get { throw new NotSupportedException(); } }
+    /// <include file='doc\XmlSerializer.uex' path='docs/doc[@for="XmlSerializerImplementation.TypedSerializers"]/*' />
+    public virtual Hashtable TypedSerializers { get { throw new NotSupportedException(); } }
+    /// <include file='doc\XmlSerializer.uex' path='docs/doc[@for="XmlSerializerImplementation.CanSerialize"]/*' />
+    public virtual bool CanSerialize(Type type) { throw new NotSupportedException(); }
+    /// <include file='doc\XmlSerializer.uex' path='docs/doc[@for="XmlSerializerImplementation.GetSerializer"]/*' />
+    public virtual XmlSerializer GetSerializer(Type type) { throw new NotSupportedException(); }
+  }
+}
