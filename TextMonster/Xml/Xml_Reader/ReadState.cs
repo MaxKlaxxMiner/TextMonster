e7e8d6f1a307bd84b29a4f566349a21fad51e46f
MaxKlaxxMiner <max@tetrisk.de>
2018-09-04 21:54:51 +0200

    925 - ReadState

---
+++ TextMonster/Xml/Xml_Reader/ReadState.cs
@@ -0,0 +1,21 @@
+﻿namespace TextMonster.Xml.Xml_Reader
+{
+  // Specifies the state of the XmlReader.
+  public enum ReadState
+  {
+    // The Read method has not been called yet.
+    Initial = 0,
+
+    // Reading is in progress.
+    Interactive = 1,
+
+    // An error occurred that prevents the XmlReader from continuing.
+    Error = 2,
+
+    // The end of the stream has been reached successfully.
+    EndOfFile = 3,
+
+    // The Close method has been called and the XmlReader is closed.
+    Closed = 4
+  }
+}
