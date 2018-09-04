8819e0e37dbd7d851c9fad81f3695d1457d3a594
MaxKlaxxMiner <max@tetrisk.de>
2018-09-04 21:51:12 +0200

    607 - XmlDateTimeSerializationMode

---
+++ TextMonster/Xml/Xml_Reader/XmlDateTimeSerializationMode.cs
@@ -0,0 +1,11 @@
+﻿namespace TextMonster.Xml.Xml_Reader
+{
+  // Options for serializing and deserializing DateTime
+  public enum XmlDateTimeSerializationMode
+  {
+    Local,
+    Utc,
+    Unspecified,
+    RoundtripKind,
+  }
+}
