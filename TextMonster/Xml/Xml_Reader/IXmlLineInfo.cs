f79a7c26b92dac5e1ee65f3f6c00e6c8d39d0af8
MaxKlaxxMiner <max@tetrisk.de>
2018-09-04 21:50:26 +0200

    623 - IXmlLineInfo

---
+++ TextMonster/Xml/Xml_Reader/IXmlLineInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace TextMonster.Xml.Xml_Reader
+{
+  /// <include file='doc\IXmlLineInfo.uex' path='docs/doc[@for="IXmlLineInfo"]/*' />
+  public interface IXmlLineInfo
+  {
+    /// <include file='doc\IXmlLineInfo.uex' path='docs/doc[@for="IXmlLineInfo.HasLineInfo"]/*' />
+    bool HasLineInfo();
+    /// <include file='doc\IXmlLineInfo.uex' path='docs/doc[@for="IXmlLineInfo.LineNumber"]/*' />
+    int LineNumber { get; }
+    /// <include file='doc\IXmlLineInfo.uex' path='docs/doc[@for="IXmlLineInfo.LinePosition"]/*' />
+    int LinePosition { get; }
+  }
+}
